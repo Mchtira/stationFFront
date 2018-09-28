7e5a57c7e6e7c51218020b2cce4b507c06338c1e
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-09-28 16:55:14 +0200

    post actual send to the api

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
-import { actions } from '../store.js'
+import { actions, store } from '../store.js'
 import { filterRooms } from '../api.js'
 
 class RoomFilter extends Component {
-  handleCheckbox = equipement => actions.handleCheckbox(equipement)
-  handleCapacity = capacity => actions.handleCapacity(capacity)
-  handleDate = date => actions.handleDate(date)
   sendForm = () => {
     const form = {
-      equipements: this.props.equipements,
-      capacity: this.props.capacity,
-      date: this.props.date,
+      equipements: store.getState().equipements,
+      capacity: store.getState().capacity,
     }
     filterRooms(form).then(actions.loadRooms)
+  }
+
+  handleCheckbox = equipement => {
+    actions.handleCheckbox(equipement)
+    this.sendForm()
+  }
+
+  handleCapacity = capacity => {
+    actions.handleCapacity(capacity)
+    this.sendForm()
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={e => e.preventDefault()}>
-          <div>
-            Capacité minimum : <input onChange={e => this.handleCapacity(e.target.value)} type='number'/>
-          </div>
-          <div>
-            <input type='checkbox' onChange={() => this.handleCheckbox('TV')}/>
-            <label>TV</label>
-            <input type='checkbox' onChange={() => this.handleCheckbox('Retro Projecteur')}/>
-            <label>Rétro Projecteur</label>
-          </div>
-          <div>
-            <input onChange={e => this.handleDate(e.target.value)} type='date'/>
-          </div>
-          <div>
-            <button onClick={this.sendForm}>Filter</button>
-          </div>
-        </form>
+        <div>
+          Capacité minimum : <input onChange={e => this.handleCapacity(e.target.value)} type='number'/>
+        </div>
+        <div>
+          <input type='checkbox' onChange={() => this.handleCheckbox('TV')}/>
+          <label>TV</label>
+          <input type='checkbox' onChange={() => this.handleCheckbox('Retro Projecteur')}/>
+          <label>Rétro Projecteur</label>
+        </div>
+        <div>
+        </div>
       </div>
     );
   }
